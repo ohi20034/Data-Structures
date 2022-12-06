fa2cfce0f5a959ba3ff45580c8c68417c1d96318
Imdadul Haque Ohi <93985458+ohi20034@users.noreply.github.com>
2022-12-06 21:20:06 +0600

    Add files via upload

---
+++ quickSort.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+int partition(int ar[],int low,int high)
+{
+    int pivot = ar[high];
+    int i =  low-1;
+    for(int j=low; j<high; ++j)
+    {
+        if(ar[j] < pivot){
+            i++;
+            swap(ar[i],ar[j]);
+        }
+    }
+    i++;
+    swap(ar[i],ar[high]);
+    return i; // return pivot index
+}
+void quick_sort(int ar[],int low,int high)
+{
+     if(low < high){
+         int pivotindex = partition(ar,low,high); // for pivot index
+         quick_sort(ar,low,pivotindex-1);
+         quick_sort(ar,pivotindex+1,high);
+     }
+}
+int main()
+{
+    /* test case
+5
+6 3 9 5 2 8
+    */
+    int n; // size of array
+    cin >> n;
+    int ar[n]; // array
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> ar[i];
+    }
+
+    quick_sort(ar,0,n-1); // sorting function
+
+    for (int i = 0; i < n; ++i)
+    {
+        cout << ar[i] << " ";
+    }
+}
