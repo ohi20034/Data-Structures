39793d958e4982484236a7879cb0b182d076e47c
Imdadul Haque Ohi <93985458+ohi20034@users.noreply.github.com>
2023-04-02 23:18:35 +0600

    Add files via upload

---
+++ Tree/trie.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TrieNode
+{
+    TrieNode *next[27];
+    bool completedWord;
+    TrieNode()
+    {
+        completedWord = false;
+        for(int i=0; i<26; ++i)
+        {
+            next[i] = NULL;
+        }
+    }
+}*root;
+
+void trieinsert(string s)
+{
+    TrieNode *curr = root;
+    for(int i=0; i<s.size(); ++i)
+    {
+        int x = s[i]-'a';
+        if(curr->next[x] == NULL)
+        {
+            curr->next[x] = new TrieNode();
+
+        }
+        else
+        {
+            curr = curr->next[x];
+        }
+    }
+    curr->completedWord = true;
+}
+bool triesearch(string s)
+{
+    TrieNode *curr = root;
+
+    for(int i=0; i<s.size(); ++i)
+    {
+        int x = s[i]-'a';
+
+        if(curr->next[x] == NULL)
+        {
+            return false;
+        }
+    }
+    return curr->completedWord;
+}
+int main()
+{
+    root = new  TrieNode();
+
+}
